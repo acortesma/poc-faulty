22dab4a04a2724ace56fc73db61467a84d2e3ee6
Angel Cortés <acortes@monom.ai>
2022-05-30 22:39:25 +0200

    Initial commit

---
+++ src/main/java/com/spring/poc/faulty/PocSpringFaultyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.poc.faulty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringFaultyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringFaultyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/poc/faulty/PocSpringFaultyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.poc.faulty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringFaultyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
